6976cf227d089cf08ec286e7187be216e6f719ba
ohhfishal <ohhfishal@gmail.com>
2025-01-11 11:40:38 -0700

    cicd: go fmt stage

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	ctx := context.Background()
+	ctx := context.Background(            )
 	if err := server.Run(ctx, os.Args, os.Getenv, os.Stdin, os.Stdout, os.Stderr); err != nil {
 		fmt.Fprintf(os.Stderr, "%s", err)
 		os.Exit(1)
